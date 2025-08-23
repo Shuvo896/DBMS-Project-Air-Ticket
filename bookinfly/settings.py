87a20956e6b5d362ab2c625fee73f428e4eebf1f
Shuvo <shuvo2305101896@diu.edu.bd>
2025-08-23 08:46:51 +0600

    deployment

--- bookinfly/settings.py
+++ bookinfly/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'yoursecretkey_here'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
